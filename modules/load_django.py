b330f31869a2b435d27b4654838857bf9aa05b80
Alexander Popov <alexander.popov.dev@gmail.com>
2026-07-02 11:38:44 +0300

    init project

---
+++ modules/load_django.py
@@ -0,0 +1,5 @@
+import sys, os, django
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'braincom_project')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'braincom_project.settings'
+django.setup()
